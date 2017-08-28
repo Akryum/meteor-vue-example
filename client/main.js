918e2b9037b7c5880bfb98bcd7de282532ce74d8
Rúnar Berg Baugsson Sigríðarson <runarberg@zoho.com>
2017-08-28 11:55:50 +0000

    Upgrade to vue 2 (#7)
    Fixes meteor-vue/vue-meteor#245

--- client/main.js
+++ client/main.js
@@ -6,11 +6,5 @@ import {Vue} from 'meteor/akryum:vue';
 import App from '/imports/ui/App.vue';
 
 Meteor.startup(() => {
-  new Vue({
-    el: 'body',
-    replace: false,
-    components: {
-      App
-    }
-  });
+  new Vue(App).$mount(document.body);
 });
